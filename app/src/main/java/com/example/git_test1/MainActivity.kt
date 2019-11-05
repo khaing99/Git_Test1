19de5ae117825387059fbfa2df4eb2983823b6b3
khaing99 <ayehninkhaing99@gmail.com>
2019-11-05 12:32:32 +0630

    log

--- app/src/main/java/com/example/git_test1/MainActivity.kt
+++ app/src/main/java/com/example/git_test1/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.git_test1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("zzh","zinzawhein")
     }
 }
